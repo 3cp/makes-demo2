c5ca3e9235ce2092595a1c6ca03f907b3972d481
Chunpeng Huo <huocp@me.com>
2019-04-30 09:50:07 +1000

    through2

--- transforms.js
+++ transforms.js
@@ -1,14 +1,11 @@
-const {Transform} = require('stream');
+const through2 = require('through2');
 
 exports.append = function(properties, features) {
-  return new Transform({
-    objectMode: true,
-    transform: function(file, env, cb) {
-      if (file.isBuffer() && file.extname === '.ext') {
-        // change .ext to .ts or .js file
-        file.extname = features.includes('typescript') ? '.ts' : '.js'
-      }
-      cb(null, file);
+  return through2.obj(function(file, env, cb) {
+    if (file.isBuffer() && file.extname === '.ext') {
+      // change .ext to .ts or .js file
+      file.extname = features.includes('typescript') ? '.ts' : '.js'
     }
+    cb(null, file);
   });
 };
